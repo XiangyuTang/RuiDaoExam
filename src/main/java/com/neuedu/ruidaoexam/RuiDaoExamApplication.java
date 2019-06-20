05311154b7b2fefcce2096895785df2077152e04
swc_20180064 <123@qq.com>
2019-06-20 15:57:37 +0800

    同步

--- src/main/java/com/neuedu/ruidaoexam/RuiDaoExamApplication.java
+++ src/main/java/com/neuedu/ruidaoexam/RuiDaoExamApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 //@ComponentScan(basePackages = {"com.neuedu.ruidaoexam.dao"})
