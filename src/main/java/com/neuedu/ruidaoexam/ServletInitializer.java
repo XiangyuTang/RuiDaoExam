fac771be5f0620d448c1bbb38b42b03755ed82e4
swc_20180064 <123@qq.com>
2019-06-22 22:07:12 +0800

    同步

--- src/main/java/com/neuedu/ruidaoexam/ServletInitializer.java
+++ src/main/java/com/neuedu/ruidaoexam/ServletInitializer.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(RuiDaoExamApplication.class);
