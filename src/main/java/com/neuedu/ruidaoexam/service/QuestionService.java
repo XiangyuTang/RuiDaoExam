de84fa321a0c3a4632fb5942d998acf06ca99d6c
HP <HP@LAPTOP-H6R29BTF>
2019-06-23 10:42:49 +0800

    实现： 1.首页显示数据栏 2.判卷页教师发布评语 3.判卷页教师下载PDF

--- src/main/java/com/neuedu/ruidaoexam/service/QuestionService.java
+++ src/main/java/com/neuedu/ruidaoexam/service/QuestionService.java
@@ -3,11 +3,6 @@ package com.neuedu.ruidaoexam.service;
 import java.util.HashMap;
 import java.util.List;
 
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import com.neuedu.ruidaoexam.entity.ChoiceQuestion;
-import com.neuedu.ruidaoexam.entity.EssayQuestion;
-import com.neuedu.ruidaoexam.entity.JudgeQuestion;
 
 public interface QuestionService {
 //	List<ChoiceQuestion> getChoiceQuestionByBankid(Integer Bank_id);
@@ -18,4 +13,5 @@ public interface QuestionService {
 	
 	//通过题库的id查询其包含的题目集合
 	List<Object> getQuestionsByBankid(Integer Bank_id);
+	
 }
---
+++ src/main/java/com/neuedu/ruidaoexam/service/ReportService.java
@@ -0,0 +1,9 @@
+package com.neuedu.ruidaoexam.service;
+
+import java.util.ArrayList;
+
+public interface ReportService {
+	void addComment(ArrayList<String> reportArray);	//教师发布报告评语
+
+
+}
