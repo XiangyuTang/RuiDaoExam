af2764d2f7aacde054a31dd842dc9cd235e5bf23
18940 <18940@DESKTOP-NOMLVQ0>
2019-07-03 18:39:26 +0800

    添加题库

--- src/main/java/com/neuedu/ruidaoexam/controller/BankController.java
+++ src/main/java/com/neuedu/ruidaoexam/controller/BankController.java
@@ -2,10 +2,14 @@ package com.neuedu.ruidaoexam.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.neuedu.ruidaoexam.entity.QuestionBank;
 import com.neuedu.ruidaoexam.service.QuestionBankService;
@@ -28,5 +32,21 @@ public class BankController {
 		}
 		model.addAttribute("banks", questionBanks);
 		return "banks";
+	}
+	@RequestMapping("/addbank")
+	@ResponseBody
+	public String addBank(String bankname,Integer banktype,HttpServletRequest request) {
+		Integer uid = (Integer) request.getSession().getAttribute("uid");
+		QuestionBank bank = new QuestionBank();
+		bank.setCreatedbyteacherid(uid);
+		bank.setQuesBankName(bankname);
+		bank.setQuesBankFiled(banktype);
+		bank.setPointPrice(0);
+		Integer addQuestBank = questionBankService.addQuestBank(bank);
+		if (addQuestBank > 0) {
+			return "true";
+		}else {
+			return "false1";
+		}
 	}
 }
