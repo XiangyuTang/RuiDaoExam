879f07e939255a3834e788e8c1d44d8adc57f7ab
swc_20180064 <123@qq.com>
2019-07-05 14:49:40 +0800

    同步

---
+++ src/main/java/com/neuedu/ruidaoexam/service/impl/PaypalService.java
@@ -0,0 +1,68 @@
+package com.neuedu.ruidaoexam.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paypal.api.payments.Amount;
+import com.paypal.api.payments.Payer;
+import com.paypal.api.payments.Payment;
+import com.paypal.api.payments.PaymentExecution;
+import com.paypal.api.payments.RedirectUrls;
+import com.paypal.api.payments.Transaction;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+import com.neuedu.ruidaoexam.config.PaypalPaymentIntent;
+import com.neuedu.ruidaoexam.config.PaypalPaymentMethod;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service("paypalService")
+public class PaypalService {
+
+    @Resource
+    private APIContext apiContext;
+
+    public Payment createPayment(
+            Double total,
+            String currency,
+            PaypalPaymentMethod method,
+            PaypalPaymentIntent intent,
+            String description,
+            String cancelUrl,
+            String successUrl) throws PayPalRESTException{
+        Amount amount = new Amount();
+        amount.setCurrency(currency);
+        amount.setTotal(String.format("%.2f", total));
+
+        Transaction transaction = new Transaction();
+        transaction.setDescription(description);
+        transaction.setAmount(amount);
+
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction);
+
+        Payer payer = new Payer();
+        payer.setPaymentMethod(method.toString());
+
+        Payment payment = new Payment();
+        payment.setIntent(intent.toString());
+        payment.setPayer(payer);
+        payment.setTransactions(transactions);
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl(cancelUrl);
+        redirectUrls.setReturnUrl(successUrl);
+        payment.setRedirectUrls(redirectUrls);
+
+        return payment.create(apiContext);
+    }
+
+    public Payment executePayment(String paymentId, String payerId) throws PayPalRESTException{
+        Payment payment = new Payment();
+        payment.setId(paymentId);
+        PaymentExecution paymentExecute = new PaymentExecution();
+        paymentExecute.setPayerId(payerId);
+        return payment.execute(apiContext, paymentExecute);
+    }
+}
